96ea244a2ebdb0ee3df46bd04a0154c7db3003cd
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-04-03 18:59:09 +0200

    modified rds instance engine version number

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_db_instance" "default" {
   identifier             = var.identifier
   allocated_storage      = var.storage
   engine                 = var.engine
-  engine_version         = var.engine_version[var.engine]
+  engine_version         = var.engine_version
   instance_class         = var.instance_class
   db_name                = var.db_name
   username               = var.username
