48b50a3782f8f160b6d5475eb8cf6c047b67b9c5
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-04-03 20:27:36 +0200

    minor changes

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,13 +1,13 @@
 # output allows terraform to share data between modules
 
-output "subnet_group" {
-  value = aws_db_subnet_group.default_subnet.name
-}
-
-output "db_instance_id" {
-  value = aws_db_instance.default.identifier
-}
-
-output "db_instance_address" {
-  value = aws_db_instance.default.address
-}
+# output "subnet_group" {
+#   value = aws_db_subnet_group.default_subnet.name
+# }
+# 
+# output "db_instance_id" {
+#   value = aws_db_instance.default.identifier
+# }
+# 
+# output "db_instance_address" {
+#   value = aws_db_instance.default.address
+# }
--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,27 +1,27 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-resource "aws_security_group" "default" {
-  name        = "spectacle stack db security group"
-  description = "Allow all inbound traffic"
-  vpc_id      = aws_default_vpc.default_vpc.id
-
-  ingress {
-    description = "http access"
-    from_port   = 1433 # assuming c# will be using port 8091
-    to_port     = 1433
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = var.sg_name
-  }
-}
+# resource "aws_security_group" "default" {
+#   name        = "spectacle stack db security group"
+#   description = "Allow all inbound traffic"
+#   vpc_id      = aws_default_vpc.default_vpc.id
+# 
+#   ingress {
+#     description = "http access"
+#     from_port   = 1433 # assuming c# will be using port 8091
+#     to_port     = 1433
+#     protocol    = "TCP"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# 
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# 
+#   tags = {
+#     Name = var.sg_name
+#   }
+# }
