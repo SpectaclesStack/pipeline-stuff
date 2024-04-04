8b7f144c2b95d4d635e558d8c771aee48e4f029b
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-04-04 13:54:51 +0200

    made few changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,7 @@
 
 provider "aws" {
   region = var.aws_region
-  # default_tags {
-  #   tags = {
-  #     owner         = "Wandile.Nyembe@bbd.co.za"
-  #     created-using = "Terraform"
-  #   }
-  # }
 }
-
-# create default vpc if one does not exist
-# resource "aws_default_vpc" "default_vpc" {
-#   tags = {
-#     Name = "default vpc"
-#   }
-# }
-
-# resource "aws_db_subnet_group" "default_subnet" {
-#   name        = "main_subnet_group"
-#   description = "Our main group of subnets"
-#   subnet_ids  = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
-# 
-#   tags   = {
-#     Name = "spectacle-stack-db-subnet"
-#   }
-# }
 
 module "db" {
   source = "terraform-aws-modules/rds/aws"
@@ -42,12 +19,11 @@ module "db" {
   port                      = 1433
   publicly_accessible       = true
   vpc_security_group_ids    = [var.vpc_security_group]
-
+  db_subnet_group_name      = "spectacles-stack-db-subnet-public1-eu-west-1a"
   multi_az = false
 
   maintenance_window        = "Mon:00:00-Mon:03:00"
   backup_window             = "03:00-06:00"
-
   backup_retention_period   = 1
   skip_final_snapshot       = true
   deletion_protection       = false
