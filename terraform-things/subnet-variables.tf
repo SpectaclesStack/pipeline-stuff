7a21f0af36faa6307125d7be717cc59dc97dc610
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-04-04 14:32:26 +0200

    renamed directories

---
+++ terraform-things/subnet-variables.tf
@@ -0,0 +1,26 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# variable "subnet_1_cidr" {
+#   default     = "10.0.0.128/26"
+#   description = "Subnet mask 1"
+# }
+# 
+# variable "subnet_2_cidr" {
+#   default     = "10.0.0.192/26"
+#   description = "Subnet mask 2"
+# }
+# 
+# variable "az_1" {
+#   default     = "eu-west-1a"
+#   description = "Your Az1, use AWS CLI to find your account specific"
+# }
+# 
+# variable "az_2" {
+#   default     = "eu-west-1b"
+#   description = "Your Az2, use AWS CLI to find your account specific"
+# }
+
+# variable "vpc_id" {
+#   description = "Your VPC ID"
+# }
---
+++ terraform-things/variables.tf
@@ -0,0 +1,73 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+# 
+# variable "aws_region" {
+#   default = "eu-west-1"
+# }
+# 
+# variable "identifier" {
+#   default     = "sectacle-stack-rds-instance"
+#   description = "Identifier for your DB"
+# }
+# 
+# variable "storage" {
+#   default     = "20"
+#   description = "Storage size in GB"
+# }
+# 
+# variable "engine" {
+#   default     = "sqlserver-ex"
+#   description = "Engine type, SQL server type"
+# }
+# 
+# variable "engine_version" {
+#   description = "Engine version"
+#   default = "16.00"
+# }
+# 
+# variable "instance_class" {
+#   default     = "db.t3.micro"
+#   description = "Instance class"
+# }
+# 
+# variable "db_name" {
+#   default     = "mydb"
+#   description = "db name"
+# }
+# 
+# variable "username" {
+#   default     = "bbdGradWandile"
+#   description = "User name"
+# }
+# 
+# variable "password" {
+#   default     = "wandile"
+#   description = "password is variable for now"
+# }
+# 
+# variable "vpc_id" {
+#   default     = "	vpc-024165bfe13c6044f"
+# }
+# 
+# variable "vpc_security_group" {
+#     default = "	sg-066606e30a5998385"
+#     description = "Db security group"
+# }
+
+# variable "vpc_security_group_two" {
+#     default = "sg-0a17fd32633a462e6"
+#     description = "Database security group"
+# }
+# 
+# variable "db_subnet_id_one" {
+#     default = "subnet-0c7083d355715cb1b"
+#     description = "value"
+# }
+# 
+# variable "db_subnet_id_two" {
+#     default = "subnet-0abbe4dbf1c33983c"
+# }
+# 
+# variable "db_availability_zone" {
+#   default = "eu-west-1a"
+# }
