9bc182a2f2db36ce3c15130e808ab41e3c63157c
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-04-04 21:14:25 +0200

    added migrations

---
+++ migrations/V002_CONSTRAINTS.sql
@@ -0,0 +1,43 @@
+--Add Constraints 
+USE [Spectables-Stack-DB]
+GO
+
+ALTER TABLE Users
+ADD CONSTRAINT UsersPK
+PRIMARY KEY(UserId);
+GO
+
+ALTER TABLE Users
+ADD CONSTRAINT UniqueEmail
+UNIQUE (Email);
+GO
+
+ALTER TABLE Users
+ADD CONSTRAINT UniqueUserName
+UNIQUE (UserName);
+GO
+
+ALTER TABLE Questions
+ADD CONSTRAINT QuestionsPK
+PRIMARY KEY(QuestionId);
+GO
+
+ALTER TABLE Questions
+ADD CONSTRAINT QuestionsFK
+FOREIGN KEY(UserId) REFERENCES Users(UserId);
+GO
+
+ALTER TABLE Answers
+ADD CONSTRAINT AnswersPK
+PRIMARY KEY(AnswerId);
+GO
+
+ALTER TABLE Answers
+ADD CONSTRAINT AnswersFK1
+FOREIGN KEY(UserId) REFERENCES Users(UserId);
+GO
+
+ALTER TABLE Answers
+ADD CONSTRAINT AnswersFK2
+FOREIGN KEY(QuestionId) REFERENCES Questions(QuestionId);
+GO
